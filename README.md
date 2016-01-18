e56933238557277f0a68adb7ff5558a16623e7e8
fengyansheng <241456911@qq.com>
2016-01-18 13:29:57 +0800

    a

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 https://hub.docker.com/r/kerngven/dockerone-crawler-client/
 
+# process env
+
+```
+	hostname=127.0.0.1:3000 #server url
+	auth_id=xe2rRYbxsa--XRjlyZVOxIkuqX4YBA8KxkM4z6aMEfg # auth docker profile
+
+```
+
--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 
 //host, userID, machineID, profileID
-// process.env['host']= '127.0.0.1';
-// process.env['port']= 80;
+// process.env['hostname']= '127.0.0.1:3000';
 // process.env['auth_id']= "xe2rRYbxsa--XRjlyZVOxIkuqX4YBA8KxkM4z6aMEfg";
-
-var server = require('./lib/server').createServer(process.env['auth_id'], process.env['host'], process.env['port']);
+var server = require('./lib/server').createServer(process.env['auth_id'], process.env['hostname']);
 
 process.on('exit', function() {
     console.log('docker exit');
