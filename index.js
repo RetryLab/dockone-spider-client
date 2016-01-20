2016bd87c636eb86086b94e6cdcf1d5e8a9c6b4d
fengyansheng <241456911@qq.com>
2016-01-20 18:29:39 +0800

    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 //host, userID, machineID, profileID
-var server = require('./lib/server').createServer(process.env['auth_id'] || 'xe2rRYbxsa--XRjlyZVOxIkuqX4YBA8KxkM4z6aMEfg', process.env['hostname'] || '114.112.58.126:3000');
+var server = require('./lib/server').createServer(process.env['auth_id'] || 'xe2rRYbxsa--XRjlyZVOxIkuqX4YBA8KxkM4z6aMEfg', process.env['hostname'] || '127.0.0.1:3000');
 
 process.on('exit', function() {
     console.log('docker exit');
